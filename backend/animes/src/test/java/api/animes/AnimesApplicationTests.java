438e252d91065e460686d3dd902e56b1137adfe2
Erika Duarte <erika.duarte.mora@gmail.com>
2024-02-20 11:56:44 -0500

    instalacion del proyecto backend con java springboot

---
+++ backend/animes/src/test/java/api/animes/AnimesApplicationTests.java
@@ -0,0 +1,13 @@
+package api.animes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
