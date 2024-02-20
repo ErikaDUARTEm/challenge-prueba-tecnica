e8cb24e496a8fe47de4565cfb6f06f665d67b53c
Erika Duarte <erika.duarte.mora@gmail.com>
2024-02-20 15:52:56 -0500

    primer enpoints que trae animes de 5 en 5

--- backend/animes/src/main/java/api/animes/AnimeApplication.java
+++ backend/animes/src/main/java/api/animes/AnimeApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnimesApplication {
+public class
+AnimeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AnimesApplication.class, args);
+		SpringApplication.run(AnimeApplication.class, args);
 	}
 
 }
