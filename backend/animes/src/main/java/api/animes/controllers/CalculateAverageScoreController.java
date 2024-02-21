11418e97c55b8d8e38a7b3bcf87b217627fd5e92
Erika Duarte <erika.duarte.mora@gmail.com>
2024-02-21 00:11:31 -0500

    modifique el carousel y el limite de animes

--- backend/animes/src/main/java/api/animes/controllers/CalculateAverageScoreController.java
+++ backend/animes/src/main/java/api/animes/controllers/CalculateAverageScoreController.java
@@ -8,19 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8080/")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class CalculateAverageScoreController {
+
     @PostMapping("/calculateAverageScore")
-    public ResponseEntity<Double> calcularPuntajePromedio(@RequestBody List<Double> scores) {
-        // Imprime los puntajes en la consola
-        System.out.println("Puntajes recibidos: " + scores);
+    public ResponseEntity<String> calcularPuntajePromedio(@RequestBody List<Double> scores) {
 
         // Realiza el cálculo del puntaje promedio
         double suma = scores.stream().mapToDouble(Double::doubleValue).sum();
         double puntajePromedio = suma / scores.size();
-
+        String mensaje = messageAccordingToScore(puntajePromedio);
         // Devuelve el puntaje promedio como parte de la respuesta
-        return ResponseEntity.ok(puntajePromedio);
+        return ResponseEntity.ok(mensaje);
+    }
+
+    private String messageAccordingToScore(double puntuacion) {
+        if (puntuacion >= 1 && puntuacion <= 4) {
+            return "No lo recomiendo.";
+        } else if (puntuacion >= 5 && puntuacion <= 7) {
+            return "Puedes divertirte.";
+        } else if (puntuacion > 7) {
+            return "Genial, esto es uno de los mejores animes.";
+        } else {
+            return "Puntuación no válida.";
+        }
     }
 }
--- frontend/src/AnimeCarousel.jsx
+++ frontend/src/AnimeCarousel.jsx
@@ -3,28 +3,33 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-
 const AnimeCarousel = () => {
-  const [animes, setAnimes] = useState([]);
+  const [animes, setAnimes] = useState(new Set());
   const [searchTerm, setSearchTerm] = useState('');
   const [scores, setScores] = useState([]);
+  const [average, setAverage] = useState();
+
+  const fetchNewAnimes = async () => {
+    try {
+      const response = await fetch('https://api.jikan.moe/v4/anime');
+      const data = await response.json();
+      console.log(data)
+      setAnimes((prevAnimes) => new Set([...prevAnimes, ...data.data]));
+    } catch (error) {
+      console.error('Error fetching new animes:', error);
+    }
+  };
 
   useEffect(() => {
-    // Lógica para obtener la lista de animes desde tu backend y actualizar el estado
-    // Ejemplo hipotético (asegúrate de adaptarlo a tu aplicación)
-    fetch('https://api.jikan.moe/v4/anime?page=1&limit=5')
-      .then((response) => response.json())
-      .then((data) =>  setAnimes(data.data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-  
-  useEffect(()=>{
-    const newScores = animes.map((item) => item.score);
+    fetchNewAnimes();
+  }, [searchTerm]);
+
+  useEffect(() => {
+    const newScores = Array.from(animes).map((item) => item.score);
     setScores((prevScores) => [...prevScores, ...newScores]);
   }, [animes]);
 
   useEffect(() => {
-    // Solo realiza la llamada al backend si scores se ha actualizado
     if (scores.length > 0) {
       fetch('http://localhost:8080/calculateAverageScore', {
         method: 'POST',
@@ -33,48 +38,46 @@ const AnimeCarousel = () => {
         },
         body: JSON.stringify(scores),
       })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
+        .then((response) => response.text())
+        .then((resultado) => {
+          console.log(resultado);
+          setAverage(resultado);
         })
-        .then(resultado => {
-          // Maneja el resultado devuelto por el backend (puntaje promedio)
-          console.log('Puntaje promedio recibido desde el backend:', resultado);
-        })
-        .catch(error => console.error('Error:', error));
+        .catch((error) => console.error('Error:', error));
     }
-  }, [scores]);
+  }, [scores, average]);
+
+  const handleArrowClick = (direction) => {
+    if (direction === 'next') {
+      setAnimes(new Set()); // Reiniciar la lista de animes al hacer clic en la flecha "next"
+      fetchNewAnimes();
+    }
+  };
 
   const settings = {
-    // Configuración del carrusel
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 5,
     slidesToScroll: 1,
+    afterChange: handleArrowClick,
   };
-  
- console.log(animes)
- console.log(scores)
- return (
-  <div>
-    <h2>API Data Carousel</h2>
-    <Slider {...settings}>
-      {animes.map((item) => (
-        <div key={item.id}>
-          {/* Contenido de cada slide */}
-          <img src={item.images.jpg.image_url} />
-          <p>{item.score}</p>
-          <h3>{item.title}</h3>
-          <p>{item.description}</p>
 
-        </div>
-      ))}
-    </Slider>
-  </div>
-);
+  return (
+    <div>
+      <h2>API Data Carousel</h2>
+      <Slider {...settings}>
+        {Array.from(animes).map((item) => (
+          <div key={item.mal_id}>
+            <img src={item.images.jpg.image_url} alt={item.title} />
+            <p>{item.score}</p>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 };
 
 export default AnimeCarousel;
