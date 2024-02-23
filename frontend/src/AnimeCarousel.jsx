a5a68ff0f2570dc24db7eaba7fc27917b75a8441
Erika Duarte <erika.duarte.mora@gmail.com>
2024-02-23 07:45:48 -0500

    documentación del codigo

--- frontend/src/AnimeCarousel.jsx
+++ frontend/src/AnimeCarousel.jsx
@@ -1,96 +1,107 @@
-import { useEffect, useState } from 'react';
-import { AnimeSlider } from './AnimeSlider';
-import { Loading } from './Loading';
+import { useEffect, useState } from "react";
+import { AnimeSlider } from "./AnimeSlider";
+import { Loading } from "./Loading";
 
-
+/*Componente que hace las llamadas a la Api Gratuita, pasa datos al backend y hace llamada al enpoint
+que contiene las reglas de negocio*/
 const AnimeCarousel = ({ searchTerm }) => {
   const [animesData, setAnimesData] = useState([]);
-  const [ loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [average, setAverage] = useState();
   const [searchError, setSearchError] = useState(null);
- 
 
-
+  //Fetch para acceder a la data de los animes
   const fetchNewAnimes = async () => {
-     try {
-      setLoading(true)
-      const response = await fetch('https://api.jikan.moe/v4/anime?page=5');
+    try {
+      setLoading(true);
+      const response = await fetch("https://api.jikan.moe/v4/anime?page=5");
       const data = await response.json();
-  
-      await new Promise(resolve => setTimeout(resolve, 3000));
-      // Actualizar el estado con los nuevos animes
-    
-      if (data && data.data && Array.isArray(data.data) && data.data.length > 0) {
-      setAnimesData((prevAnimes) => [...prevAnimes, ...data.data]);
-       } else {
-        console.error('La respuesta de la API no tiene el formato esperado:', data);
+
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      // Actualización de el estado con los nuevos animes
+      if (
+        data &&
+        data.data &&
+        Array.isArray(data.data) &&
+        data.data.length > 0
+      ) {
+        setAnimesData((prevAnimes) => [...prevAnimes, ...data.data]);
+      } else {
+        console.error(
+          "La respuesta de la API no tiene el formato esperado:",
+          data
+        );
       }
-      setSearchError(null); // Limpiar cualquier error previo
+      setSearchError(null); // Limpia cualquier error previo
     } catch (error) {
-      console.error('Error fetching new animes:', error);
+      console.error("Error fetching new animes:", error);
       setSearchError(error);
-    }finally {
+    } finally {
       setLoading(false); // Establecer loading en false al finalizar la solicitud
     }
-}
+  };
   useEffect(() => {
     fetchNewAnimes();
   }, [searchTerm]);
 
-// Lógica para enviar datos al backend
-useEffect(() => {
-  const fetchAverageScore = async () => {
-    setLoading(true);
-    const scoresPorTitulo = {};
-  
-    animesData.forEach(anime => {
-      const titulo = anime.title;
-  
-      if (!scoresPorTitulo[titulo]) {
-        scoresPorTitulo[titulo] = [];
-      }
-  
-      scoresPorTitulo[titulo].push(anime.score || 0);
-    });
-  
-    const scores = {
-      scoresPorTitulo: scoresPorTitulo,
+  // Lógica para enviar datos al backend
+  useEffect(() => {
+    const fetchAverageScore = async () => {
+      setLoading(true);
+      const scoresPorTitulo = {};
+
+      animesData.forEach((anime) => {
+        const titulo = anime.title;
+
+        if (!scoresPorTitulo[titulo]) {
+          scoresPorTitulo[titulo] = [];
+        }
+
+        scoresPorTitulo[titulo].push(anime.score || 0);
+      });
+
+      const scores = {
+        scoresPorTitulo: scoresPorTitulo,
+      };
+      //Fetch al enpoint que devuelve las reglas de negocio
+      const response = await fetch(
+        "http://localhost:8080/calculateAverageScore",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(scores),
+        }
+      );
+      const data = await response.json();
+      setAverage(data);
+      setLoading(false);
     };
-  
-    const response = await fetch('http://localhost:8080/calculateAverageScore', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      
-      body: JSON.stringify(scores),
-    })       
-  
-    const data = await response.json()
-    setAverage(data)
-    setLoading(false)
-  }
+    fetchAverageScore();
+  }, [animesData]);
 
-  fetchAverageScore()
-  
-}, [animesData]);
-
-
-// Filtro de los animes según búsqueda
- const getFilteredAnimes = (animes) => {
-  return animes.filter((anime) => {
-    const titleLowerCase = anime.title.toLowerCase();
-    const searchTermLowerCase = searchTerm.toLowerCase();
-    return titleLowerCase.includes(searchTermLowerCase);
-  });
-};
-const filteredAnimesForSlider = getFilteredAnimes(animesData);
-
+  // Filtro de los animes según búsqueda
+  const getFilteredAnimes = (animes) => {
+    return animes.filter((anime) => {
+      const titleLowerCase = anime.title.toLowerCase();
+      const searchTermLowerCase = searchTerm.toLowerCase();
+      return titleLowerCase.includes(searchTermLowerCase);
+    });
+  };
+  const filteredAnimesForSlider = getFilteredAnimes(animesData);
   return (
     <>
-      {loading ? <Loading/> :
-      <AnimeSlider loading={loading} filteredAnimes={filteredAnimesForSlider} averageData={average}/>
-      }
+      {loading ? (
+        <Loading />
+      ) : (
+        <AnimeSlider
+          loading={loading}
+          filteredAnimes={filteredAnimesForSlider}
+          averageData={average}
+        />
+      )}
     </>
   );
 };
--- frontend/src/AnimeSlider.jsx
+++ frontend/src/AnimeSlider.jsx
@@ -3,8 +3,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./assets/style.css";
-import { Loading } from "./Loading";
 
+//Componente Slider con su configuración y estructura de renderizado
 export const AnimeSlider = ({ loading, filteredAnimes, averageData }) => {
   const settings = {
     dots: false,
@@ -19,16 +19,16 @@ export const AnimeSlider = ({ loading, filteredAnimes, averageData }) => {
     responsive: [
       {
         breakpoint: 1024,
-          settings: {
-            mobileFirst: true,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 5,
-            slidesToScroll: 5,
-            centerMode: false,
-            variableWidth: true,
-            focusOnSelect: true
-          }
+        settings: {
+          mobileFirst: true,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          centerMode: false,
+          variableWidth: true,
+          focusOnSelect: true,
+        },
       },
       {
         breakpoint: 768,
@@ -48,35 +48,30 @@ export const AnimeSlider = ({ loading, filteredAnimes, averageData }) => {
     ],
   };
 
-  console.log('average de Slider ', averageData)
-
   return (
     <div className="slider-container">
-   
-     <Slider {...settings}>
-      {filteredAnimes.map((anime) => {
-        return (
-          <div key={anime.mal_id} className="container-cards">
-            <div className="card">
-              <img
-                src={anime.images?.jpg?.image_url}
-                alt={anime.title}
-                className="img-animes"
-              />
-              <h3>{anime.title}</h3>
-             {averageData &&
-                averageData.puntajesPromedio &&
-                averageData.clasificaciones &&
-                averageData.puntajesPromedio[anime.title] && (
-                  <p>{averageData.clasificaciones[anime.title]}</p>
-                )}
+      <Slider {...settings}>
+        {filteredAnimes.map((anime) => {
+          return (
+            <div key={anime.mal_id} className="container-cards">
+              <div className="card">
+                <img
+                  src={anime.images?.jpg?.image_url}
+                  alt={anime.title}
+                  className="img-animes"
+                />
+                <h3>{anime.title}</h3>
+                {averageData &&
+                  averageData.puntajesPromedio &&
+                  averageData.clasificaciones &&
+                  averageData.puntajesPromedio[anime.title] && (
+                    <p>{averageData.clasificaciones[anime.title]}</p>
+                  )}
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </Slider>
+          );
+        })}
+      </Slider>
     </div>
   );
 };
-
-export default AnimeSlider;
